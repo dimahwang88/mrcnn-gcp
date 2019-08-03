7e608ebb8cb595bc16f45b25cc61326e5c4b7752
Dmitriy Khvan <dmitriy.khvan@Dmitriys-MBP.fritz.box>
2019-08-03 18:01:01 +0200

    min confidence

--- mrcnn/config.py
+++ mrcnn/config.py
@@ -177,7 +177,7 @@ class Config(object):
 
     # Minimum probability value to accept a detected instance
     # ROIs below this threshold are skipped
-    DETECTION_MIN_CONFIDENCE = 0.6
+    DETECTION_MIN_CONFIDENCE = 0.3
 
     # Non-maximum suppression threshold for detection
     DETECTION_NMS_THRESHOLD = 0.5
